aaff02b054753a55c7b0d99e20b47dd137b2e8ce
Sarah Paetsch <spaetsch@gmail.com>
2015-06-30 18:28:35 -0700

    Updated comments

--- js/flickrTest.js
+++ js/flickrTest.js
@@ -8,10 +8,13 @@ var search = function(){
     tags: $flickrSearch,
     format: "json"
   };
+  //search runs, but currently we only return fillmurray stock photos
+  //so we know what size we're getting
   function loadPhotos(data) {
     var viewer = '<ul class="bxslider">';
     for (var i=0; i < data.items.length; i++){
       viewer = viewer +  '<li><img src="http://www.fillmurray.com/480/360"></li>'
+      //this commented out code will insert search results from public Flickr feed
       // viewer = viewer + '<li><img src="' + data.items[i].media.m +
       // '" title="by ' + data.items[i].author + '"></li>';
       // console.log(data.items[i]);
@@ -21,7 +24,7 @@ var search = function(){
     $('.bxslider').bxSlider({
       pager: true,
       pagerType:'short', //use numbers instead of dots
-      captions: true
+      captions: true    //will show captions from text in title field of <img>
     });
   }
   $.getJSON(flickrAPI, options, loadPhotos);
@@ -31,7 +34,6 @@ $('#search-form').submit(function(event) {
   clear();
   search();
 });
-
 
 $(document).ready(function(){ //run on load with stock photos
   $('.bxslider').bxSlider();
